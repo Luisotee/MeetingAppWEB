38a8c7308ea452a6b62273812d673969bfc83d06
Luisotee <luisotaviomgr1@gmail.com>
2022-11-10 22:37:21 -0300

    desktop ready

--- components/main-card/right-side/right-side.tsx
+++ components/main-card/right-side/right-side.tsx
@@ -1,5 +1,28 @@
 import { Title } from "@mantine/core";
+import { Calendar, RangeCalendar, TimeRangeInput } from "@mantine/dates";
+import dayjs from "dayjs";
+import { useState } from "react";
 
 export function RightSide() {
-  return <Title order={3}>Select a date and time</Title>;
+  const [dates, setDates] = useState<[Date | null, Date | null]>([
+    new Date(2021, 11, 1),
+    new Date(2021, 11, 5),
+  ]);
+
+  const now = new Date();
+  const then = dayjs(now).add(30, "minutes").toDate();
+  const [value, setValue] = useState<[Date, Date]>([now, then]);
+
+  return (
+    <>
+      <Title order={3}>Select a date and time</Title>
+      <RangeCalendar value={dates} onChange={setDates} mt="md" />
+      <TimeRangeInput
+        label="Appointment time"
+        value={value}
+        onChange={setValue}
+        clearable
+      />
+    </>
+  );
 }
