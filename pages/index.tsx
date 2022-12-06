f3323914795847165b3ed5973e66bede1f599f08
Luisotee <luisotaviomgr1@gmail.com>
2022-12-06 00:02:09 -0300

    Just to test

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         }
 
         if (
-          meeting.limitData < dateNow &&
+          meeting.limitData == dateNow &&
           meeting.bestTime == null &&
           meeting.choosenTimes != null
         ) {
